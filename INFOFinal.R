4af8876ba0a986b9c08df8468f25ee209a1810a0
Lishen-Mao <lmao@emich.edu>
2017-12-07 01:03:19 -0500

    Tried to create a raster stack and extract raster value to point in R but got some strange result. The extracted values are all 32766 which means null so I switched to Arcgis and got MODIS GPP value for flux tower at grandbay

--- INFOFinal.R
+++ INFOFinal.R
@@ -1,3 +1,21 @@
 #Downloading MODIS GPP data 
 library (MODIStsp)
 MODIStsp()
+
+# load necessary packages
+
+library(raster)
+library(sp)
+library(rgdal)
+#
+setwd('C:/Users/Lishen Mao/Documents/INFO 8000 Assignment 3/Final_INFO/INFOFinal/Gross_PP_8Days_1Km_v5/GPP/')
+# boundary shapefile for flux tower at grand bay
+GrandBay <- readOGR(dsn = ".", "Flux_tower_repro")
+#go through all tif flies and save to GBM1000_2015_GB_forGPP
+getwd()
+GBM1000_2015_GB_forGPP <- list.files(pattern="*.tif")
+#create a raster stack for all tif files
+GBM1000_2015_GB_stack <- lapply(GBM1000_2015_GB_forGPP, raster)
+#extract GPP value for flux tower pixel.
+GBM1000_2015_GB_extract <- lapply(GBM1000_2015_GB_stack, extract, GrandBay)
+#switch to arcgis to extract values to flux tower pixel
